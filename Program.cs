71eea75838a07aa11a4355f37792190cfea96f41
ester <ester@ZETTERLUND>
2023-09-07 13:42:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace DataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Ester";
+            //// Värdetyp
+            //int x = 15;
+            //int y = x;
+            //x = 30;
+            //Console.WriteLine(value: y);
+
+            //// Referenstyper
+            //StringBuilder strb1 = new StringBuilder(value: "hej");
+            //StringBuilder strb2 = strb1;
+            //strb1.AppendLine();
+            //strb1.Append("Hur mår du?");
+
+            //Console.WriteLine(strb2.ToString());
+
+
+
+        }
+    }
+}
